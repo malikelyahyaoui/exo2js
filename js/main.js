fdf7e6a7e90a6ba497ba44d60295e7c67f5e2a30
malikelyahyaoui <elyahyaoui.malik67@gmail.com>
2024-11-07 10:07:24 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,11 +1,9 @@
 // Sélectionne tous les div qui se trouve dans carre-haut
 const carreHaut = document.querySelectorAll('.carre-haut > div');
 
+
 // Sélectionne l'élément avec la classe carrebas
 const carreBas = document.querySelector('.carrebas');
-
-// Sélectionne  carrebas pour afficher la couleur en texte
-const carreBasCouleur = document.querySelector('.carrebas');
 
 // Parcourt chaque div dans carre-haut
 carreHaut.forEach(square => {
@@ -14,10 +12,17 @@ carreHaut.forEach(square => {
         // Récupère la couleur de fond du carrreé cliqué
         const color = window.getComputedStyle(square).backgroundColor;
 
+        // getBoundingClientRect() pour obtenir les dimensions du carré cliqué
+        const { width,height } = square.getBoundingClientRect();
+
         // Change la couleur de fond du carré au carré cliqué
         carreBas.style.backgroundColor = color;
 
-        // Affiche la couleur de fond dans le carre qui est en bas
-        carreBasCouleur.textContent = color;
+       
+        
+        // Afficher toutes les donnés
+
+        carreBas.textContent =`Couleur: ${color}, Largeur: ${(width)}px, Hauteur: ${(height)}px`;
+
     });
 });
